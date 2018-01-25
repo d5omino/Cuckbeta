271b51be74ad9a21736702312e292e4ac60184a6
Joey Freyre <jose.freyre@gmail.com>
2018-01-25 13:06:26 -0500

    initia

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 using WebApplication1.Data;
 using WebApplication1.Models;
 using WebApplication1.Services;
@@ -16,6 +15,9 @@ namespace WebApplication1
 {
     public class Startup
     {
+        public string DevDbString { get; set; }
+        public string ProdDbString { get; set; }
+        public string EnvironmentIn { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,23 +28,41 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            DevDbString = Configuration["DevDb"];
+            ProdDbString = Configuration[Environment.GetEnvironmentVariable("ProdDbString")];
+            EnvironmentIn = Configuration[Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")];
+
+
+            if(EnvironmentIn.Equals("Development"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration[DevDbString]));
+            }
+
+            if(EnvironmentIn.Equals("Production"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration[ProdDbString]));
+            }
+
+
+
+
+            services.AddIdentity<ApplicationUser,IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddTransient<IEmailSender,EmailSender>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if(env.IsDevelopment())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
